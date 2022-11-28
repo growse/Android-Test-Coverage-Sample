310d8ed4a277ec35364a8bd8872e3558edb6a002
Andrew Rowson <github@growse.com>
2022-11-28 16:11:06 +0000

    Gradle scan

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,3 +12,10 @@ buildscript {
         // in the individual module build.gradle files
     }
 }
+
+if (hasProperty("buildScan")) {
+    extensions.findByName("buildScan")?.withGroovyBuilder {
+        setProperty("termsOfServiceUrl", "https://gradle.com/terms-of-service")
+        setProperty("termsOfServiceAgree", "yes")
+    }
+}
